d1dec811e73bbc19baaca4e300780e5fc396b8a2
Visshwasmai <visshwasmaipulishetti@gmail.com>
2026-03-06 11:38:29 +0530

    Update registration API endpoint URL

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -19,7 +19,7 @@ function Reg()
       {
         //  const res=await axios.post("http://localhost:8080/reg",data)
         const res = await axios.post(
-        "https://tt-3u4n.onrender.com/reg",
+        "https://newprojectrender-0riv.onrender.com",
         data
       )
          alert(res.data)
